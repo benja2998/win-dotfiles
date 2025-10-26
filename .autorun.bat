f8a400cd48e0c57c4990800f638ad0b2db414c07
benja2998 <benja2998@duck.com>
2025-10-26 17:39:13 -0300

    Add alias.bat script and improve GNU tool detection
    Introduces scripts/alias.bat for managing command aliases with help text and usage examples. Updates .autorun.bat to detect GNU tools and conditionally set doskey aliases for which, rm, mv, and cp based on their availability.

--- .autorun.bat
+++ .autorun.bat
@@ -26,20 +26,28 @@ doskey sudo=gsudo $*
 doskey su=gsudo
 doskey sudo.ti=gsudo --ti $* & rem TrustedInstaller
 doskey sudo.system=gsudo -s $* & rem SYSTEM
-doskey alias=doskey /macros
 doskey vim=nvim $*
 doskey clear=cls
 doskey clr=cls
-doskey rm=del /Q $*
-doskey mv=move $*
-doskey cp=copy $*
 doskey cat=bat -pP $*
 doskey ls=eza --color=auto $*
 doskey ll=eza -lha --color=auto $*
 doskey la=eza -a --color=auto $*
 doskey l=eza -lh --color=auto $*
-doskey which=where $*
 doskey e.=explorer .
+
+set NO_GNU=0
+which --version >nul 2>&1 || set NO_GNU=1
+rm --version >nul 2>&1 || set NO_GNU=1
+mv --version >nul 2>&1 || set NO_GNU=1
+cp --version >nul 2>&1 || set NO_GNU=1
+
+if %NO_GNU%==1 (
+    doskey which=where $*
+    doskey rm=del /Q $*
+    doskey mv=move $*
+    doskey cp=copy $*
+)
 
 rem Modify PATH
 set "PATH=%~dp0scripts;%PATH%"
---
+++ scripts/alias.bat
@@ -0,0 +1,34 @@
+@rem alias.bat
+
+@echo off
+
+set "ESC="
+
+if "%~1"=="" (
+    doskey /macros
+    exit /b
+) else (
+    if "%~1"=="/?" (
+        echo %ESC%[1;4mHelp text for alias%ESC%[0m
+        echo.
+        echo alias /?         : Display this help message
+        echo alias name=value : Create an alias
+        echo alias            : Show defined aliases
+        echo.
+        echo %ESC%[1;4mCodes used for defining aliases%ESC%[0m
+        echo.
+        echo $T               : Command separator
+        echo $1-9             : Parameters passed to the alias
+        echo $*               : All parameters passed to the alias. Never use this!
+        echo.
+        echo %ESC%[1;4mExample usage of alias%ESC%[0m
+        echo.
+        echo $ alias test=cls
+        echo $ cls /?
+        cls /?
+    ) else if "%~1"=="/fullhelp" (
+        doskey /?
+    ) else (
+        doskey %* $*
+    )
+)
